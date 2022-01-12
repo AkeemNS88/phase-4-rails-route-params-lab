e4574d6fa3a0af4e4a46cbfd9ab22b23bcd1440d
Akeem <AkeemS11788@gmail.com>
2022-01-12 11:35:03 -0500

    commit tree 5d1b0d993e4c56802198eae3fe7f142dc1685ce9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,18 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name]
+      #students = Student.where(first_name: params[:name]) #|| (last_name: params[:name]))
+     # byebug
+     students = Student.all.filter{|student| student.first_name.downcase.include?(params[:name]) || student.last_name.downcase.include?(params[:name])}
+     # students = Student.where('last_name like ?', params[:name])
+    end
+    render json: students
+  end
+    
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
